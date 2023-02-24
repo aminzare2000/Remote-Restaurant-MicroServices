b7c4a96bf9c16e64621c1dd47aac3dc8acbbbaec
aminzare <amin.zare@gmail.com>
2023-02-25 00:14:21 +0330

    Init Basket.API

---
+++ src/Services/Basket/Basket.API/Entities/Basket.cs
@@ -0,0 +1,33 @@
+﻿namespace Basket.API.Entities
+{
+    public class Basket
+    {
+        public string Username { get; set; }
+        
+        public List<BasketItem> Items { get; set;} = new List<BasketItem>();
+
+        public Basket()
+        {
+
+        }
+
+        public Basket(string username)
+        {
+            Username = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal price = 0;
+                foreach (var item in Items)
+                {
+                    price += item.Price*item.Weight;
+                }
+
+                return price;
+            }
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Entities/BasketItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Entities
+{
+    public class BasketItem
+    {
+        public decimal Price { get; set; }
+        public decimal Weight { get; set; }
+        public string IngredientName { get; set; }
+        public int IngredientId { get; set; }
+    }
+}
